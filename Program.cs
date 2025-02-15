bc0abb550346efa691e75ddd8422b5c4190b1c4c
20100 <mmmelkady23@gmail.com>
2025-02-15 23:00:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linq_Session_one_Not_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
